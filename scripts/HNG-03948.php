b33623732c87cabe80705e0aeb14c11ab6d03a7f
unknown <lanzer09@gmail.com>
2020-06-03 10:33:40 +0100

    stage1

---
+++ scripts/HNG-03948.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG-03948</title>
+</head>
+<body>
+	<?php
+
+		$id = 'HNG-03948';
+
+		echo "<p>Hello World, this is Lanre Yusuf with HNGi7 ID $id using PHP for stage 2. Lanzer09@gmail.com task</p>"
+
+	?>
+</body>
+</html>
